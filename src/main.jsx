838de5307f9f6f8f2ec6629e0ecc5db4786afe0c
buuninbee <buunin.bee@gmail.com>
2026-01-30 23:14:50 -0300

    feat: criando as rotas da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,25 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css"
 
 import App from './App.jsx'
+import Assunto from './pages/Assunto';
+import Resumo from './pages/Resumo';
+import Identificacao from './pages/Identificacao';
+import Anexo from './pages/Anexo';
+import Login  from './pages/Login';
+import Protocolo from './pages/Protocolo';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
-      <Route index element={<App />} />
-        
+      <Route index path='/' element={<App />} />
+      <Route path='/assunto' element={<Assunto />} />
+      <Route path='/resumo' element={<Resumo />} />
+      <Route path='/identificacao' element={<Identificacao />} />
+      <Route path='/anexo' element={<Anexo />} />
+      <Route path='/protocolo' element={<Protocolo />} />
+      <Route path='/login' element={<Login />} />
+
     </Routes>
     </BrowserRouter>
   </StrictMode>,
